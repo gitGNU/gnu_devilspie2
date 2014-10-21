d8f6fb8aaf04eb571dc36ff3c16ed857d69a7dc7
Andreas Rönnquist <gusnan@gusnan.se>
2014-10-21 06:20:45 +0200

    Remove trailing whitespace

--- src/config.h
+++ src/config.h
@@ -2,8 +2,8 @@
  *	This file is part of devilspie2
  *	Copyright (C) 2013 Andreas Rönnquist
  *
- *	devilspie2 is free software: you can redistribute it and/or 
- *	modify it under the terms of the GNU General Public License as published 
+ *	devilspie2 is free software: you can redistribute it and/or
+ *	modify it under the terms of the GNU General Public License as published
  *	by the Free Software Foundation, either version 3 of the License, or
  *	(at your option) any later version.
  *
@@ -13,7 +13,7 @@
  *	GNU General Public License for more details.
  *
  *	You should have received a copy of the GNU General Public License
- *	along with devilspie2.  
+ *	along with devilspie2.
  *	If not, see <http://www.gnu.org/licenses/>.
  */
 #ifndef __HEADER_CONFIG_
